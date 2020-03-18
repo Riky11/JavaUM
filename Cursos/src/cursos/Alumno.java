01114e8879e6ec03cdcf4ae333984a29c3447793
Riky <Riky@DESKTOP-A82KRRQ.home>
2020-03-18 12:48:29 +0100

    Ejercicio Cursos -> Clase Alumno y Curso

---
+++ Cursos/src/cursos/Alumno.java
@@ -0,0 +1,60 @@
+package cursos;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Alumno {
+	// Atributos
+	private final String nombre;
+	private final String dni;
+	private int credito;
+	private Set<Curso> cursosMatriculados;
+	
+	// Contructores
+	public Alumno(String nombre, String dni) {
+		this.nombre = nombre;
+		this.dni = dni;
+		this.credito = 100;
+		this.cursosMatriculados = new HashSet<Curso>();
+	}
+	
+	// Consultas de los atributos
+	public Alumno(String nombre, String dni, int credito) {
+		this.nombre = nombre;
+		this.dni = dni;
+		this.credito = credito;
+		this.cursosMatriculados = new HashSet<Curso>();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public int getCredito() {
+		return credito;
+	}
+
+	public Set<Curso> getCursosMatriculados() {
+		return cursosMatriculados;
+	}
+
+	// Incrementar credito
+	public void IncrementarCredito(int cantidad) {
+		this.credito += cantidad; 
+	}
+	
+	// Decrementar credito
+	public void DecrementarCredito(int cantidad) {
+		this.credito -= cantidad; 
+	}
+	
+	public void AddCurso(Curso c) {
+		this.cursosMatriculados.add(c);
+	}	
+	
+	
+}
---
+++ Cursos/src/cursos/Curso.java
@@ -0,0 +1,88 @@
+package cursos;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+public abstract class Curso {
+	private String titulo;
+	private Date inicico;
+	private Date finalizacion;
+	private int numDias;
+	private int precioMatricula;
+	private Set<Alumno> alumnosMatriculados;
+	private Set<Alumno> alumnosAptos;
+	private int numMatricludaos;
+	
+	public Curso(String titulo, Date inicio, Date finalizacion, int numDias, int preciMatricula) {
+		this.titulo = titulo;
+		this.inicico = inicio;
+		this.finalizacion = finalizacion;
+		this.numDias = numDias;
+		this.precioMatricula = preciMatricula;
+		this.alumnosMatriculados = new HashSet<Alumno>();
+		this.alumnosAptos = new HashSet<Alumno>();
+		this.numMatricludaos = 0;
+	}
+	
+	//Metodos para consultar propiedades
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public Date getInicico() {
+		return inicico;
+	}
+
+	public Date getFinalizacion() {
+		return finalizacion;
+	}
+
+	public int getNumDias() {
+		return numDias;
+	}
+
+	public int getPrecioMatricula() {
+		return precioMatricula;
+	}
+
+	public Set<Alumno> getAlumnosMatriculados() {
+		return alumnosMatriculados;
+	}
+
+	public Set<Alumno> getAlumnosAptos() {
+		return alumnosAptos;
+	}
+
+	public int getNumMatricludaos() {
+		return numMatricludaos;
+	}
+	
+	// Funciones
+	public boolean Haterminado() {
+		Date actual = new Date();
+		if(actual.compareTo(finalizacion) > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean EsApto(Alumno a) {
+		return this.alumnosAptos.contains(a);
+	}
+	
+	public boolean Matriculacion(Alumno a) {
+		if(a.getCredito() > this.precioMatricula) {
+			a.DecrementarCredito(this.precioMatricula);
+			this.alumnosMatriculados.add(a);
+			a.AddCurso(this);
+			return true;
+		}
+		return false;
+	}
+	
+	protected abstract boolean calificar();	
+	
+}
